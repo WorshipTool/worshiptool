7820d34de77b25bee5e914ddeefeeba18cd9095e
Petr Pavlín <pavlip17@fit.cvut.com>
2022-12-27 01:12:26 +0100

    gr: first router use

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
-import { Typography } from '@mui/material';
+import { ThemeProvider, Typography } from '@mui/material';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './pages/Home/Home';
 
 function App() {
-  return (
-    <Typography>Hey.</Typography>
-  );
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element: <Home/>,
+      errorElement: <Typography>Error.</Typography>
+    }
+  ]);
+  
+    return (
+      
+      <RouterProvider router={router}/>
+    )
 }
 
 export default App;
