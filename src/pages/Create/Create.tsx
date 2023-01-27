aa3fa96701a634e41597d1f4e830b0c65bdff5cd
Petr Pavlín <pavlip17@fit.cvut.com>
2023-01-27 18:00:08 +0100

    gr: Create page better design

--- src/pages/Create/Create.tsx
+++ src/pages/Create/Create.tsx
@@ -1,5 +1,5 @@
-import { Box, Button, InputBase, Typography, styled, useTheme } from '@mui/material'
-import React, { useEffect, useRef, useState } from 'react'
+import { Box, Button, Divider, FormControlLabel, InputBase, Switch, Tooltip, Typography, styled, useTheme } from '@mui/material'
+import React, { useCallback, useEffect, useRef, useState } from 'react'
 import AddIcon from '@mui/icons-material/Add';
 import CloseIcon from '@mui/icons-material/Close';
 import CircularProgress from '@mui/material/CircularProgress';
@@ -13,47 +13,69 @@ import Toolbar from '../../components/Toolbar';
 import DefaultStyle from '../Sheet/styles/DefaultStyle';
 import Song, { Variant } from '../../models/song';
 import convertSheetToSections from '../../api/conversition/convertSheetToSections';
+import Gap from '../../components/Gap';
+import ToolPanel from './ToolPanel';
+
+const Container = styled(Box)(({theme})=>({
+    width: "70%",
+    margin: theme.spacing(5),
+    display:"flex",
+    flexDirection:"column"
+}))
+
+const StyledContainer = styled(Box)(({theme})=>({
+    padding: theme.spacing(3),
+    backgroundColor: theme.palette.grey[100],
+    boxShadow: `0px 0px 5px ${theme.palette.grey[400]}`,
+    display:"flex"
+}))
+
+const TitleInput = styled(InputBase)(({theme})=>({
+    fontWeight: theme.typography.fontWeightBold
+}))
+const SheetInput = styled(InputBase)(({})=>({
+    height:200,
+    justifyContent:"start",
+    alignItems:"start"
+}))
 
 export default function Create() {
-    const theme = useTheme();
-
-    const [titleValue, setTitleValue] = useState("");
-    const [altTitleValue, setAltTitleValue] = useState("");
-    const [titles, setTitles] = useState<string[]>([]);
-    const [sheetValue, setSheetValue] = useState("");
-
-    const [altTitleTyping, setAltTitleTyping] = useState(false);
-
     const [posting, setPosting] = useState(false);
 
-    const sheetInputRef = useRef(null);
-    const titleInputRef = useRef(null);
+    const [preview, setPreview] = useState(false);
+
+    const sheetInputRef :any = useRef(null);
+    const titleInputRef :any = useRef(null);
+
+    const [title, setTitle] = useState("");
+    const [sheet, setSheet] = useState("");
 
     const navigate = useNavigate()
 
     const getSong = () : Song => {
         return {
-            title: titleValue,
+            title: title,
             variants: [{
                 guid: "",
-                sheetData: sheetValue,
+                sheetData: sheet,
                 sheetText: "",
-                sections: convertSheetToSections(sheetValue),
+                sections: convertSheetToSections(sheet),
                 preferredTitle: "",
                 createdBy: "",
                 verified: false
             }],
             guid: "",
-            alternativeTitles: titles,
+            alternativeTitles: [title],
             creators:[]
         };
     }
 
     const [song, setSong] = useState<Song>(getSong());
 
+
     useEffect(()=>{
         setSong(getSong());
-    },[titleValue, sheetValue])
+    },[title, sheet])
     
 
     const {post, loading:fetching} = useFetch();
@@ -62,84 +84,16 @@ export default function Create() {
         if(!fetching) setPosting(fetching);
     },[fetching])
 
-
-
-    const onTitleValueChange = (event: any) => {
-        setTitleValue(event.target.value);
-    }
-    const onAltTitleValueChange = (event: any) => {
-        setAltTitleValue(event.target.value);
-    }
-    const onSheetValueChange = (event: any) => {
-        setSheetValue(event.target.value);
-    }
-
-    const onAddAltTitleClick = () => {
-        setAltTitleTyping(true);
-    }
-
-    const onTitleInputKeyDown = (event: any) => {
-        if(event.key==="Enter"){
-            setMainTitle();
-            
-        }
-        if(event.key==="Escape"){
-            
-        }
-    }
-    const onTitleInputKeyUp = (event:any)=>{
-        const sheetInput : any = sheetInputRef.current;
-        if(event.key==="Enter"){
-            sheetInput.focus();
-
-        }
-    }
-
-    const setMainTitle = () => {
-        const newTitles : string[] = [...titles];
-
-        if(newTitles.length==0) newTitles.push("");
-
-        newTitles[0] = titleValue;
-
-        setTitles(newTitles);
-
-    }
-    const onAltTitleInputKeyDown = (event: any) => {
-        if(event.key==="Enter"){
-            addAltTitle();
-        }
-        if(event.key==="Escape"){
-            blurAltTitleInput();
-        }
-    }
-    const onAltTitleInputBlur = () => {
-        if(altTitleValue==""){
-            blurAltTitleInput();
-            return;
-        }
-        addAltTitle();
-    }
-    const blurAltTitleInput = () => {
-        setAltTitleTyping(false);
-        setAltTitleValue("");
-    }
-    const addAltTitle = () => {
-        setTitles([...titles, altTitleValue]);
-        setAltTitleTyping(false);
-        setAltTitleValue("");
-    }
-
     const onPostClick = () => {
+        const song = getSong();
         setPosting(true);
         const dto = convertSongToNewSongDTO(
             convertNewSongDataToSong({
-                title: titleValue, 
-                alternativeTitles: titles.length<2?[]:titles.slice(1, titles.length-1), 
-                sheet: sheetValue}));
+                title: song.title, 
+                alternativeTitles: song.alternativeTitles, 
+                sheet: song.variants[0].sheetData}));
 
         post({url: getUrl_POSTNEWSONG(), body: dto}, (d:RequestResult<any>)=> {
-            console.log(d);
             if(isSuccess(d)){
                 if(d.data.guid){
                     navigate(`/song/`+d.data.guid, { replace: false })
@@ -148,123 +102,95 @@ export default function Create() {
         });
     }
 
-
-    const isAltTitlesEnabled = () => {
-        return titles.length>0&&titles[0]!="";
+    const newSection = (name:string) => {
+        const target = sheetInputRef.current;
+        let textToInsert = `{${name}}`
+        let cursorPosition = target.selectionStart
+        let textBeforeCursorPosition = target.value.substring(0, cursorPosition)
+        let textAfterCursorPosition = target.value.substring(cursorPosition, target.value.length)
+        setSheet(textBeforeCursorPosition + textToInsert + textAfterCursorPosition);
+        target.focus();
     }
 
-    const RemoveAltIcon = styled(CloseIcon)(()=>({
-        fontSize:"inherit",
-        marginTop:0.1,
-        marginLeft:0.2,
-        color:theme.palette.grey[500],
-        '&:hover':{
-            color:"black"
-        }
-        }))
-
-    const AltTitle = ({index,title,onRemove}:{index:number,title:string, onRemove:()=>void}) => {
-        return (
-            <Box sx={{padding: 1, paddingTop: 0.3, paddingBottom: 0.3, bgcolor: theme.palette.grey[300], borderRadius: 1,
-                "&:hover":{
-                    bgcolor:theme.palette.grey[400]
-                }}}>
-                <Typography variant={"caption"} sx={{fontWeight: "300", fontSize: 13, alignItems:"center", display:"flex"}}>
-                    {title}
-                    {!posting&&<RemoveAltIcon onClick={onRemove}/>}
-                </Typography>
-            </Box>
-        
-        )
+    const newChord = () => {
+        const target = sheetInputRef.current;
+        let textToInsert = "[C]"
+        let cursorPosition = target.selectionStart
+        let textBeforeCursorPosition = target.value.substring(0, cursorPosition)
+        let textAfterCursorPosition = target.value.substring(cursorPosition, target.value.length)
+        setSheet(textBeforeCursorPosition + textToInsert + textAfterCursorPosition);
+        target.focus();
     }
 
-    const styledContainerSX = {
-        margin:3,
-        marginBottom:0,
-        padding:3,
-        backgroundColor: theme.palette.grey[100],
-        borderStyle: "solid",
-        borderWidth: 1,
-        borderColor: theme.palette.grey[400],
-        borderRadius: 1,
-        flex:1,
-        display:"flex",
-        flexDirection: "column"
-    }
-    const titleInputSX = {
-        width:"100%",
-        fontSize: 25,
-        fontWeight: "300"
-    }
+    const keysHandler = useCallback(
+        (event:any) => {
+            if (event.altKey === true) {
+                if(event.key==='c'){
+                    newChord();
+                    
+                }
 
-    const postButtonContainerSX = {
-        margin: 3
-    }
+                if(event.key==='v'){
+                    newSection("V");
+                }
+                if(event.key==='r'){
+                    newSection("R");
+                }
+                if(event.key==='b'){
+                    newSection("B");
+                }
+              }
+        },[song]
+    )
+
     return (
         <Box display={"flex"} flexDirection={"column"} height={"100vh"}>
             <Toolbar transparent={false}/>
             <Box flex={1} display={"flex"} flexDirection={"row"}>
-                <Box sx={{flex:1, display:"flex", flexDirection:"column"}}>
-                    <Box sx={styledContainerSX}> 
-                        <InputBase inputRef={titleInputRef} placeholder='Zadej název písně' value={titleValue} sx={titleInputSX} size={"medium"}
-                            onChange={onTitleValueChange} onKeyDown={onTitleInputKeyDown} onKeyUp={onTitleInputKeyUp} onBlur={setMainTitle} autoFocus/>
-            
-                        
-                        {isAltTitlesEnabled()&&(
-            
-                            <Box sx={{display:"flex", gap: 1}}>
-            
-                                {titles.filter((t, index)=>index!=0).map((title, index)=>{    
-                                    const onRemove = () => {
-                                        setTitles([
-                                            ...titles.slice(0, index),
-                                            ...titles.slice(index + 1)
-                                          ]);
-                                    }
-            
-                                    return (
-                                        <AltTitle index={index} title={title} onRemove={onRemove} key={"title"+index}/>
-                                    )
-                                })}
-            
-                                {altTitleTyping&&
-                                    <InputBase placeholder='Nový podnázev' sx={{fontWeight: "300"}} size={"small"} 
-                                        onKeyDown={onAltTitleInputKeyDown} value={altTitleValue} onChange={onAltTitleValueChange} 
-                                        onBlur={onAltTitleInputBlur} autoFocus/>
-                                }
-                
-                                {!altTitleTyping&&!posting&&
-                                    <Button sx={{color:"black"}} size={"small"} onClick={onAddAltTitleClick}>
-                                        <Typography variant="caption" sx={{alignItems:"center", display:"flex"}}>
-                                            <AddIcon fontSize='inherit' sx={{marginRight:0.5}}/>
-                                            PŘIDEJ PODNÁZEV
-                                        </Typography>
-                                    </Button>
-                                }   
-                            </Box>
-            
-                        )}
-            
-                        <Box sx={{flex:1, display:"flex", marginTop:2}}>
-                            <InputBase inputRef={sheetInputRef} placeholder='Zde je místo na obsah písně' multiline 
-                                sx={{flex:1, alignItems:"start"}} value={sheetValue} onChange={onSheetValueChange}/>
+                <Box sx={{flex:1, display:"flex", flexDirection:"column", justifyContent:"start", alignItems:"center"}}>
+                    
+                    <Container>
+
+                        <Box display={"flex"} justifyContent={"end"} padding={1}>   
+                            <Typography display={"flex"} alignItems={"center"} variant='caption'>Náhled</Typography>                                 
+                            <Switch size='small' checked={preview} onChange={(e)=>{setPreview((p)=>!p)}}/>
                         </Box>
+
+                        {!preview?
+                            <StyledContainer>
+                                    
+                                   <Box display={"flex"} flexDirection={"column"} flex={1}>
+                                        <TitleInput placeholder='Zadejte název písně' inputRef={titleInputRef}
+                                            value={title} onChange={(e)=>{setTitle(e.target.value)}}/>
+    
+                                        <SheetInput placeholder='Zde je místo pro obsah písně...' inputRef={sheetInputRef} multiline
+                                            value={sheet} onChange={(e)=>{setSheet(e.target.value)}} onKeyDown={keysHandler}/>
+                                   </Box>
+
+                                   <ToolPanel onNewSection={newSection} onNewChord={newChord}/>
             
-                        
-                    </Box>
+                            </StyledContainer>
+                            :
+                            <StyledContainer flexDirection={"column"}>
+                                {(song.title==""&&song.variants[0].sections.length==0)&&<Typography variant="caption">Tady uvidite ukazku...</Typography>}
+                                    <DefaultStyle song={song} variant={song.variants[0]}/>
+                            </StyledContainer>
+                        }
+
+                        <Gap/>
+                        <Box display={"flex"} justifyContent={"start"}>
+                            <Tooltip title={"Přidat"}>
+                                <Button variant={"contained"} color={"primary"} disabled={posting||(song.title==""||song.variants[0].sheetData=="")} onClick={onPostClick}> 
+                                    Ověřit a přidat
+                                    {posting&& <CircularProgress color={"inherit"} size={16} sx={{marginLeft:1}}/> }
+                                </Button>
+                            </Tooltip>                            
+                        </Box>
+                    </Container>
         
-                    <Box sx={postButtonContainerSX}>
-                        <Button variant={"contained"} color={"primary"} disabled={posting} onClick={onPostClick}> 
-                            Ověřit a přidat
-                            {posting&& <CircularProgress color={"inherit"} size={16} sx={{marginLeft:1}}/> }
-                        </Button>
-                    </Box>
+                    
                     
         
-                </Box>
-                <Box sx={{...styledContainerSX, margin:0}}>
-                    {(song.title==""&&song.variants[0].sections.length==0)&&<Typography variant="caption">Tady uvidite ukazku...</Typography>}
-                    <DefaultStyle song={song} variant={song.variants[0]}/>
                 </Box>
             </Box>
         </Box>
