1557655ee79e03978af52b2e1a0b61869fbbbc5e
Petr Pavlín <pe.pavlin@gmail.com>
2024-05-09 14:52:42 +0200

    try: how helmet title works with share

--- src/pages/Test/Test.tsx
+++ src/pages/Test/Test.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useSmartNavigate } from "../../routes";
 import { Button, IconButton } from "../../common/ui";
+import { Helmet } from "react-helmet";
 
 export default function Test() {
     const onClick = (e: any) => {};
@@ -11,6 +12,9 @@ export default function Test() {
                 padding: 20
             }}>
             <IconButton onClick={onClick}>Ahoj</IconButton>
+            <Helmet>
+                <title>Testoidd</title>
+            </Helmet>
         </div>
     );
 }
