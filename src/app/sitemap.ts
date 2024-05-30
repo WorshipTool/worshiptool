9754b561283338a8cddf369a48e3ee97d999952e
Petr Pavlín <pe.pavlin@gmail.com>
2024-05-30 19:28:14 +0200

    Remove upload page from sitemap.xml

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -65,12 +65,12 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 			changeFrequency: 'yearly',
 			priority: 0.6,
 		},
-		{
-			url: `${BASE_URL}${routesPaths['upload']}`,
-			lastModified: date,
-			changeFrequency: 'yearly',
-			priority: 0.6,
-		},
+		// {
+		// 	url: `${BASE_URL}${routesPaths['upload']}`,
+		// 	lastModified: date,
+		// 	changeFrequency: 'yearly',
+		// 	priority: 0.6,
+		// },
 
 		...(songsMap as any),
 	]
