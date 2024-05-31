67959cc1a46d52e5fd3f248deda5964007e71857
Petr Pavlín <pe.pavlin@gmail.com>
2024-05-31 13:41:28 +0200

    Next try to fix GA

--- src/app/components/components/GoogleAnalytics.tsx
+++ src/app/components/components/GoogleAnalytics.tsx
@@ -1,24 +1,10 @@
-import Script from 'next/script'
+import { GoogleAnalytics as Ga } from '@next/third-parties/google'
 
 export const GoogleAnalytics = () => {
 	const MEASUREMENT_ID = 'G-1BHSYS3YY2'
 	return (
 		<>
-			<Script
-				strategy="lazyOnload"
-				src={`https://www.googletagmanager.com/gtag/js?id=${MEASUREMENT_ID}`}
-			/>
-
-			<Script id="" strategy="lazyOnload">
-				{`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${MEASUREMENT_ID}', {
-              page_path: window.location.pathname,
-              });
-          `}
-			</Script>
+			<Ga gaId={MEASUREMENT_ID} />
 		</>
 	)
 }
