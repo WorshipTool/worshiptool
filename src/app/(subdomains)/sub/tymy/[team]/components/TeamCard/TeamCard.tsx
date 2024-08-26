1893db21358dc83c25adf86818e15140b0c2a09d
Petr Pavlín <pe.pavlin@gmail.com>
2024-08-26 10:54:05 +0200

    feature: TeamCard template

---
+++ src/app/(subdomains)/sub/tymy/[team]/components/TeamCard/TeamCard.tsx
@@ -0,0 +1,26 @@
+import { Typography } from '@/common/ui/Typography'
+import { Box } from '@mui/material'
+import React from 'react'
+
+type TeamCardProps = {
+	title?: string
+	children?: React.ReactNode
+}
+
+export default function TeamCard(props: TeamCardProps) {
+	return (
+		<Box
+			sx={{
+				padding: 3,
+				borderRadius: 3,
+				bgcolor: 'grey.100',
+				// boxShadow: 1,
+			}}
+		>
+			<Typography variant="h6" strong>
+				{props.title}
+			</Typography>
+			{props.children}
+		</Box>
+	)
+}
--- src/app/(subdomains)/sub/tymy/[team]/page.tsx
+++ src/app/(subdomains)/sub/tymy/[team]/page.tsx
@@ -1,14 +1,28 @@
 'use client'
+import TeamCard from '@/app/(subdomains)/sub/tymy/[team]/components/TeamCard/TeamCard'
 import { TeamPageTitle } from '@/app/(subdomains)/sub/tymy/[team]/components/TopPanel/components/TeamPageTitle'
 import { PageProps } from '@/common/types'
 import { useClientPathname } from '@/hooks/pathname/useClientPathname'
+import { Grid } from '@mui/material'
 
 export default function TeamPage(props: PageProps<'team'>) {
 	const pathname = useClientPathname()
 	return (
 		<div>
 			<TeamPageTitle>Přehled</TeamPageTitle>
-			ahojj {props.params.team} {pathname}
+
+			<Grid container spacing={2}>
+				<Grid item sm={12} md={6} lg={4}>
+					<TeamCard title="Nejbližší akce">
+						ahojj {props.params.team} {pathname}
+					</TeamCard>
+				</Grid>
+				<Grid item sm={12} md={6} lg={4}>
+					<TeamCard title="Připnuté">
+						ahojj {props.params.team} {pathname}
+					</TeamCard>
+				</Grid>
+			</Grid>
 		</div>
 	)
 }
