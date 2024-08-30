cc2afdf1c344db528de2b15d9c5fa63d4f2df6e3
Petr Pavlín <pe.pavlin@gmail.com>
2024-08-30 13:17:14 +0200

    feature

---
+++ src/app/(subdomains)/sub/tymy/[alias]/zpevnik/components/SearchFieldTeamZpevnik.tsx
@@ -0,0 +1,10 @@
+import SearchBar from '@/common/ui/SearchBar/SearchBar'
+import { Box } from '@mui/material'
+
+export default function SearchFieldTeamZpevnik() {
+	return (
+		<Box maxWidth={500}>
+			<SearchBar />
+		</Box>
+	)
+}
--- src/app/(subdomains)/sub/tymy/[alias]/zpevnik/page.tsx
+++ src/app/(subdomains)/sub/tymy/[alias]/zpevnik/page.tsx
@@ -1,18 +1,41 @@
 'use client'
 import { TeamPageTitle } from '@/app/(subdomains)/sub/tymy/[alias]/components/TopPanel/components/TeamPageTitle'
+import SearchFieldTeamZpevnik from '@/app/(subdomains)/sub/tymy/[alias]/zpevnik/components/SearchFieldTeamZpevnik'
 import useInnerTeam from '@/app/(subdomains)/sub/tymy/hooks/useInnerTeam'
+import SongListCards from '@/common/components/songLists/SongListCards/SongListCards'
+import { Button } from '@/common/ui/Button'
+import { Typography } from '@/common/ui/Typography'
 import usePlaylist from '@/hooks/playlist/usePlaylist'
+import { Add } from '@mui/icons-material'
+import { Box } from '@mui/system'
+import { useMemo } from 'react'
 
 export default function TeamSongsPage() {
 	const { selectionGuid } = useInnerTeam()
-	const { items } = usePlaylist(selectionGuid)
+	const { items: playlistItems } = usePlaylist(selectionGuid)
+	const items = useMemo(() => {
+		return playlistItems.map((item) => item.variant)
+	}, [playlistItems])
+
 	return (
 		<div>
 			<TeamPageTitle>Zpěvník</TeamPageTitle>
-			pisnee <i>{selectionGuid}</i>
-			{items.map((item) => (
-				<div key={item.guid}>{item.variant.preferredTitle}</div>
-			))}
+
+			<Box display={'flex'} flexDirection={'column'}>
+				{items.length > 0 && <SearchFieldTeamZpevnik />}
+
+				<SongListCards data={items} />
+				{items.length === 0 && (
+					<Box gap={1} display={'flex'} flexDirection={'column'}>
+						<Typography>Ve zpěvníku týmu nejsou žádné písně...</Typography>
+						<Box display={'flex'}>
+							<Button startIcon={<Add />} size="small" color="primarygradient">
+								Přidat píseň
+							</Button>
+						</Box>
+					</Box>
+				)}
+			</Box>
 		</div>
 	)
 }
