0216946ceaab920d1ad6d0025ed5cfdf947b9d3b
Petr Pavlín <pe.pavlin@gmail.com>
2024-09-14 14:10:44 +0200

    feature: basic transpose in team

--- src/app/(subdomains)/sub/tymy/[alias]/pisen/[hex]/[title-alias]/components/SongPreview.tsx
+++ src/app/(subdomains)/sub/tymy/[alias]/pisen/[hex]/[title-alias]/components/SongPreview.tsx
@@ -1,26 +1,82 @@
 import { SongVariantDto } from '@/api/dtos'
+import TransposePanel from '@/app/(layout)/pisen/[hex]/[alias]/components/TransposePanel'
 import SheetDisplay from '@/common/components/SheetDisplay/SheetDisplay'
 import { Box } from '@mui/material'
 import { Sheet } from '@pepavlin/sheet-api'
+import { useState } from 'react'
 
 type SongPreviewProps = {
-	sheet: Sheet
+	// sheet: Sheet
 	variant: SongVariantDto
 }
 
 export default function SongPreview({ variant, ...props }: SongPreviewProps) {
+	const [inEditMode, setInEditMode] = useState(false)
+
+	const [sheet, setSheet] = useState<Sheet>(variant.sheet)
+
+	const [justNumber, setJustNumber] = useState(0)
+	const rerender = () => {
+		setJustNumber(justNumber + 1)
+	}
+
 	return (
 		<Box
 			sx={{
 				padding: 4,
 				bgcolor: 'grey.100',
 				borderRadius: 2,
+				position: 'relative',
+				display: 'flex',
+				flexDirection: 'column',
+				gap: 2,
 			}}
 		>
+			{sheet.getKeyChord() && (
+				<Box display={'flex'}>
+					<TransposePanel
+						disabled={false}
+						transpose={function (i: number): void {
+							sheet.transpose(i)
+							rerender()
+							// throw new Error('Function not implemented.')
+						}}
+					/>
+				</Box>
+			)}
+			{/* <Box
+				display={'flex'}
+				flexDirection={'row'}
+				justifyContent={'end'}
+				sx={{
+					position: 'absolute',
+					right: 4 * 8,
+					width: '30%',
+				}}
+			>
+				{!inEditMode ? (
+					<Button
+						color="secondary"
+						size="small"
+						onClick={() => setInEditMode(true)}
+					>
+						Upravit
+					</Button>
+				) : (
+					<Button
+						color="secondary"
+						size="small"
+						onClick={() => setInEditMode(false)}
+					>
+						Uložit
+					</Button>
+				)}
+			</Box> */}
 			<SheetDisplay
-				sheet={props.sheet}
+				sheet={sheet}
 				hideChords={false}
-				// title={variant.preferredTitle}
+				editMode={inEditMode}
+				title={inEditMode ? variant.preferredTitle : undefined}
 			/>
 		</Box>
 	)
--- src/app/(subdomains)/sub/tymy/[alias]/pisen/[hex]/[title-alias]/page.tsx
+++ src/app/(subdomains)/sub/tymy/[alias]/pisen/[hex]/[title-alias]/page.tsx
@@ -10,8 +10,6 @@ import { SmartParams } from '@/routes'
 import { useApiStateEffect } from '@/tech/ApiState'
 import { AudioFile } from '@mui/icons-material'
 import { Box } from '@mui/material'
-import { Sheet } from '@pepavlin/sheet-api'
-import { useState } from 'react'
 
 type TeamPisenPageProps = {
 	params: SmartParams<'teamSong'>
@@ -19,12 +17,6 @@ type TeamPisenPageProps = {
 
 export default function TeamPisenPage(props: TeamPisenPageProps) {
 	// const {} = useSmartParams
-	const [sheet, setSheet] = useState<Sheet | null>(null)
-
-	const [justNumber, setJustNumber] = useState(0)
-	const rerender = () => {
-		setJustNumber(justNumber + 1)
-	}
 
 	const [apiState] = useApiStateEffect(async () => {
 		const alias = getVariantAliasFromParams(
@@ -34,7 +26,6 @@ export default function TeamPisenPage(props: TeamPisenPageProps) {
 		const v = await getVariantByAlias(alias)
 		const variant = v.variants[0]
 		const d = mapSongDataVariantApiToSongVariantDto(variant)
-		setSheet(d.sheet)
 		return d
 	}, [props.params.hex, props.params['title-alias']])
 
@@ -70,9 +61,9 @@ export default function TeamPisenPage(props: TeamPisenPageProps) {
 					{apiState.data?.preferredTitle}
 				</Box>
 			</TeamPageTitle>
-			{apiState.data && sheet && (
+			{apiState.data && (
 				<>
-					<SongPreview variant={apiState.data} sheet={sheet} />
+					<SongPreview variant={apiState.data} />
 				</>
 			)}
 		</Box>
