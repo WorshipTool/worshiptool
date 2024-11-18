3969e10aaeb26a07bc14516713c35f6251daabe2
Petr Pavlín <pe.pavlin@gmail.com>
2024-11-18 15:34:15 +0100

    feature: better public team page settings

--- src/app/(submodules)/(teams)/sub/tymy/(teampage)/[alias]/nastaveni/components/AdvancedSettings.tsx
+++ src/app/(submodules)/(teams)/sub/tymy/(teampage)/[alias]/nastaveni/components/AdvancedSettings.tsx
@@ -1,7 +1,7 @@
 import TeamCard from '@/app/(submodules)/(teams)/sub/tymy/(teampage)/[alias]/components/TeamCard/TeamCard'
 import { useTeamPayload } from '@/app/(submodules)/(teams)/sub/tymy/(teampage)/hooks/payload/useTeamPayload'
 import useInnerTeam from '@/app/(submodules)/(teams)/sub/tymy/(teampage)/hooks/useInnerTeam'
-import { Checkbox } from '@/common/ui'
+import { Box, Checkbox, Typography } from '@/common/ui'
 import { useEffect, useState } from 'react'
 
 export default function AdvancedSettings() {
@@ -21,13 +21,30 @@ export default function AdvancedSettings() {
 		setValue(_value.showSongbookForNotMembers)
 	}, [_value.showSongbookForNotMembers])
 	return (
-		<TeamCard title="Pokročilé nastavení">
-			<Checkbox
-				label={'Zobrazit seznam písní pro nečleny'}
-				checked={value}
-				onChange={onChange}
-				disabled={loading}
-			/>
+		<TeamCard>
+			<Box
+				display={'flex'}
+				justifyContent={'space-between'}
+				flexDirection={'column'}
+				flexWrap={'wrap'}
+				gap={1}
+			>
+				<Box maxWidth={500}>
+					<Typography variant={'h6'} strong>
+						Co se má zobrazit lidem, kteří nejsou členy?
+					</Typography>
+					<Typography color="grey.600">
+						Lidé mimo tým, mohou být rovnou přesměrováni pryč, nebo jim může být
+						zobrazen alespoň seznam písní.
+					</Typography>
+				</Box>
+				<Checkbox
+					label={'Zobrazit seznam písní pro lidi mimo tým'}
+					checked={value}
+					onChange={onChange}
+					disabled={loading}
+				/>
+			</Box>
 		</TeamCard>
 	)
 }
