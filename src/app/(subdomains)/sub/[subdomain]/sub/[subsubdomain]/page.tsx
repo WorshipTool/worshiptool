f7a680ed361eed1bce3c5b2ca865f30cb6eca75c
Petr Pavlín <pe.pavlin@gmail.com>
2024-11-25 11:02:09 +0100

    better icons

--- src/app/(subdomains)/sub/[subdomain]/sub/[subsubdomain]/page.tsx
+++ src/app/(subdomains)/sub/[subdomain]/sub/[subsubdomain]/page.tsx
@@ -1,6 +1,16 @@
 import { SmartPage } from '@/common/components/app/SmartPage/SmartPage'
+import { Box, Button } from '@/common/ui'
 
 export default SmartPage(SubdomainPage)
 function SubdomainPage() {
-	return <div>nashle</div>
+	return (
+		<Box display={'flex'} flexDirection={'column'} padding={4} gap={2}>
+			Při přesměrování nastala chyba...
+			<Box display={'flex'}>
+				<Button variant="contained" to="home">
+					Jít na domovskou stránku
+				</Button>
+			</Box>
+		</Box>
+	)
 }
