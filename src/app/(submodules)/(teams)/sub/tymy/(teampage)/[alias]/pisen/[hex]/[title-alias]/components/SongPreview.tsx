fe0718d2dede1cbcee13908540f63cc275ffbb86
Petr Pavlín <pe.pavlin@gmail.com>
2024-11-29 14:20:19 +0100

    fix: team song editmode pael width

--- src/app/(submodules)/(teams)/sub/tymy/(teampage)/[alias]/pisen/[hex]/[title-alias]/components/SongPreview.tsx
+++ src/app/(submodules)/(teams)/sub/tymy/(teampage)/[alias]/pisen/[hex]/[title-alias]/components/SongPreview.tsx
@@ -17,6 +17,7 @@ import HeartLikeButton from '@/common/ui/SongCard/components/HeartLikeButton'
 import { useApi } from '@/hooks/api/useApi'
 import useAuth from '@/hooks/auth/useAuth'
 import { usePermission } from '@/hooks/permissions/usePermission'
+import { useSmartUrlState } from '@/hooks/urlstate/useUrlState'
 import { useSmartParams } from '@/routes/useSmartParams'
 import { useApiState } from '@/tech/ApiState'
 import { handleApiCall } from '@/tech/handleApiCall'
@@ -31,8 +32,8 @@ type SongPreviewProps = {
 export default function SongPreview({ variant }: SongPreviewProps) {
 	const { guid: teamGuid } = useInnerTeam()
 
-	const { edit, 'title-alias': titleAlias, hex } = useSmartParams('teamSong')
-	const [inEditMode, setInEditMode] = useState(edit || false)
+	const { 'title-alias': titleAlias, hex } = useSmartParams('teamSong')
+	const [inEditMode, setInEditMode] = useSmartUrlState('teamSong', 'edit')
 
 	const hasPermissionToEdit = usePermission<TeamPermissions>(
 		'team.edit_songs',
@@ -95,6 +96,7 @@ export default function SongPreview({ variant }: SongPreviewProps) {
 				)
 			},
 			() => {
+				setInEditMode(false)
 				if (newData.title) window.location.reload()
 			}
 		)
@@ -188,7 +190,7 @@ export default function SongPreview({ variant }: SongPreviewProps) {
 							<Box>
 								{hasPermissionToEdit && (
 									<EditButtonsPanel
-										inEditMode={inEditMode}
+										inEditMode={Boolean(inEditMode)}
 										setInEditMode={setInEditMode}
 										variant={variant}
 										onSave={onSave}
@@ -219,11 +221,15 @@ export default function SongPreview({ variant }: SongPreviewProps) {
 					flexWrap={'wrap'}
 					gap={2}
 				>
-					<Box>
+					<Box
+						sx={{
+							flex: inEditMode ? 1 : undefined,
+						}}
+					>
 						<SheetDisplay
 							sheet={sheet}
 							hideChords={hideChords}
-							editMode={inEditMode}
+							editMode={Boolean(inEditMode)}
 							title={inEditMode ? variant.preferredTitle : undefined}
 							onChange={onSheetChange}
 						/>
