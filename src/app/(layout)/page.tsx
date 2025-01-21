ace1882e53a66ae15bbd39ba53ab82e70abb68ca
Petr Pavlín <pe.pavlin@gmail.com>
2025-01-21 11:19:16 +0100

    add parse admin option to home screen

--- src/app/(layout)/page.tsx
+++ src/app/(layout)/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 
+import ParseAdminOption from '@/app/(layout)/vytvorit/components/ParseAdminOption'
 import HomeDesktop from '@/app/components/HomeDesktop'
+import useBottomPanel from '@/app/providers/BottomPanelProvider'
 import { SmartPage } from '@/common/components/app/SmartPage/SmartPage'
 import { useEffect } from 'react'
 import useGroup from '../../hooks/group/useGroup'
@@ -13,10 +15,18 @@ export default SmartPage(Home, {
 
 function Home() {
 	const { turnOff } = useGroup()
+	const p = useBottomPanel()
 
 	useEffect(() => {
 		turnOff()
+		p.setHeight(70) //TODO: this is because of admin options... do it better
 	}, [])
 
-	return <HomeDesktop />
+	return (
+		<>
+			<HomeDesktop />
+
+			<ParseAdminOption />
+		</>
+	)
 }
