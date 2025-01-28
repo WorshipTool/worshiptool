9955238674146f5a56e37492b4e3eab1f23451fa
Petr Pavlín <pe.pavlin@gmail.com>
2025-01-28 11:37:36 +0100

    fix flags logger

--- src/common/providers/FeatureFlags/flags.tech.ts
+++ src/common/providers/FeatureFlags/flags.tech.ts
@@ -4,15 +4,12 @@ import * as configcat from 'configcat-js'
 import { User } from 'configcat-js'
 
 import dotenv from 'dotenv'
-import { isDevelopment } from '../../../tech/development.tech'
 dotenv.config()
 
 export const configcatApiKey = process.env.NEXT_PUBLIC_CONFIGCAT_API_KEY
 
 export const checkFlag = async (key: FeatureFlag): Promise<boolean> => {
-	const logger = isDevelopment
-		? configcat.createConsoleLogger(configcat.LogLevel.Info)
-		: undefined
+	const logger = configcat.createConsoleLogger(configcat.LogLevel.Error)
 
 	const configCatClient = configcat.getClient(
 		configcatApiKey,
