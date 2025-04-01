956d85758cc043e7a3104b40b6443a05dc72f413
Petr Pavlín <pe.pavlin@gmail.com>
2025-04-01 23:50:45 +0200

    chore: update Playwright config for conditional web server command and add wait timeout in tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,12 +1,16 @@
 import { defineConfig } from '@playwright/test'
 
 import dotenv from 'dotenv'
+import fs from 'fs'
 dotenv.config()
 
 export default defineConfig({
 	testDir: './tests/e2e',
 	webServer: {
-		command: 'npm run build && npm run start',
+		command: fs.existsSync('.next/routes-manifest.json')
+			? 'npm run start'
+			: 'npm run build && npm run start',
+		// command: 'npm run dev',
 		port: 5500,
 		reuseExistingServer: !process.env.CI,
 		timeout: 120 * 1000, // 2 minutes
--- tests/e2e/pages/teams/tools.page.spec.ts
+++ tests/e2e/pages/teams/tools.page.spec.ts
@@ -9,6 +9,8 @@ test('Team is in tools', async ({ page }) => {
 	await page.getByRole('button', { name: 'Nástroje' }).click()
 	await page.getByRole('link', { name: 'Zkouska Zkouska' }).click()
 
+	await page.waitForTimeout(3000)
+
 	await expect(page.url()).toContain('tymy/ahtk3wx')
 
 	await expect(page.getByRole('link', { name: 'Zkouska' })).toBeVisible()
