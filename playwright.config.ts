b116dff337d1d06f91331a0fa5aaa616f68684e7
Petr Pavlín <pe.pavlin@gmail.com>
2025-07-18 11:58:46 +0200

    chore: restructure webServer configuration for better CI handling

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,18 +4,24 @@ import dotenv from 'dotenv'
 import fs from 'fs'
 dotenv.config()
 
+const isCI = !!process.env.CI
 export default defineConfig({
 	testDir: './tests/e2e',
-	webServer: {
-		command: fs.existsSync('.next/routes-manifest.json')
-			? 'npm run start'
-			: 'npm run build && npm run start',
-		port: 5500,
-		reuseExistingServer: !process.env.CI,
-		timeout: 120 * 1000, // 2 minutes
-		// "stdout": "ignore",
-		stderr: 'ignore',
-	},
+	...(isCI
+		? {}
+		: {
+				webServer: {
+					command:
+						false && fs.existsSync('.next/routes-manifest.json') // just trying
+							? 'npm run start'
+							: 'npm run build && npm run start',
+					port: 5500,
+					reuseExistingServer: true,
+					timeout: 120 * 1000, // 2 minutes
+					// "stdout": "ignore",
+					stderr: 'ignore',
+				},
+		  }),
 	use: {
 		baseURL: process.env.NEXT_PUBLIC_FRONTEND_URL,
 		headless: true,
